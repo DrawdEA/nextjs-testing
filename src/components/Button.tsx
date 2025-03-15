aaefb3cb5d29f3d9f04b3e0e23ed044a0cdcc191
Edward Joshua Diesta <99942301+DrawdEA@users.noreply.github.com>
2025-03-15 13:47:19 +0800

    Add page switching

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Button({ text }: { text: string }) {
     return (
-    <button className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">
-        {text}
-    </button>
+        <Link href="/game">
+            <button className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">
+                {text}
+            </button>
+        </Link>
     );
 }
