333f2c8f5d4428c3ce2605b7a74dd6ef35af4115
Edward Joshua Diesta <99942301+DrawdEA@users.noreply.github.com>
2025-03-21 21:42:07 +0800

    Add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 
 import Button from "../components/Button";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Image 
+        src="/logo.png"
+        alt="Logo"
+        width={800}
+        height={450}
+      />
       <Link href="/game">
         <Button text="START" onClick={() => console.log("STARTING!") } />
       </Link>
